859c12af2d396a9f7f51b42684c218c843473e7a
Victoria <bernard.victoria22@gmail.com>
2019-06-06 13:06:37 -0500

    readme edit and prettier

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Retrieve the dominant color of a PNG or JPEG image.
 
-A template for kick starting a Cloudflare worker project.
-
 `index.js` is the content of the Workers script.
 
 #### Wrangler
@@ -13,3 +11,5 @@ To generate using [wrangler](https://github.com/cloudflare/wrangler)
 ```
 wrangler generate myApp https://github.com/xtuc/img-color-worker
 ```
+
+See live demo at: https://workers-tooling.cf/demos/templates/img-color
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-import { PNG } from "pngjs/browser";
-import str from "string-to-stream";
-var jpeg = require("jpeg-js");
+import { PNG } from 'pngjs/browser'
+import str from 'string-to-stream'
+var jpeg = require('jpeg-js')
 
 function base64ToArrayBuffer(base64) {
-  var binary_string = window.atob(base64);
-  var len = binary_string.length;
-  var bytes = new Uint8Array(len);
+  var binary_string = window.atob(base64)
+  var len = binary_string.length
+  var bytes = new Uint8Array(len)
   for (var i = 0; i < len; i++) {
-    bytes[i] = binary_string.charCodeAt(i);
+    bytes[i] = binary_string.charCodeAt(i)
   }
-  return bytes.buffer;
+  return bytes.buffer
 }
 
-addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event));
-});
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event))
+})
 
 async function handleRequest({ request }) {
   try {
-    if (request.method === "GET") {
+    if (request.method === 'GET') {
       const res = new Response(`
       <!DOCTYPE html>
       <html>
@@ -53,60 +53,54 @@ async function handleRequest({ request }) {
           </script>
         </body>
       </html>
-    `);
-      res.headers.set("content-type", "text/html");
-      return res;
+    `)
+      res.headers.set('content-type', 'text/html')
+      return res
     }
 
-    if (request.method === "POST") {
-      const [data, base64] = (await request.text()).split(",");
+    if (request.method === 'POST') {
+      const [data, base64] = (await request.text()).split(',')
 
-      if (data === "data:image/jpeg;base64") {
-        var rawImageData = jpeg.decode(base64ToArrayBuffer(base64));
-        return new Response(
-          meanRgba(rawImageData.width, rawImageData.height, rawImageData.data)
-        );
+      if (data === 'data:image/jpeg;base64') {
+        var rawImageData = jpeg.decode(base64ToArrayBuffer(base64))
+        return new Response(meanRgba(rawImageData.width, rawImageData.height, rawImageData.data))
       }
 
-      if (data === "data:image/png;base64") {
+      if (data === 'data:image/png;base64') {
         return await new Promise(ok => {
           str(base64ToArrayBuffer(base64))
             .pipe(
               new PNG({
-                filterType: 4
+                filterType: 4,
               })
             )
-            .on("parsed", function() {
-              ok(new Response(meanRgba(this.width, this.height, this.data)));
-            });
-        });
+            .on('parsed', function() {
+              ok(new Response(meanRgba(this.width, this.height, this.data)))
+            })
+        })
       }
 
-      return new Response("unsupported: " + data, { status: 400 });
+      return new Response('unsupported: ' + data, { status: 400 })
     }
 
-    return new Response("not found", { status: 404 });
+    return new Response('not found', { status: 404 })
   } catch (err) {
-    console.log(err);
-    return new Response(err.stack, { status: 500 });
+    console.log(err)
+    return new Response(err.stack, { status: 500 })
   }
 }
 
 function meanRgba(w, h, matrix) {
-  const size = w * h;
-  const rgb = [0, 0, 0];
+  const size = w * h
+  const rgb = [0, 0, 0]
   for (var y = 0; y < h; y++) {
     for (var x = 0; x < w; x++) {
-      var idx = (w * y + x) << 2;
-      rgb[0] += matrix[idx];
-      rgb[1] += matrix[idx + 1];
-      rgb[2] += matrix[idx + 2];
+      var idx = (w * y + x) << 2
+      rgb[0] += matrix[idx]
+      rgb[1] += matrix[idx + 1]
+      rgb[2] += matrix[idx + 2]
     }
   }
 
-  return [
-    Math.floor(rgb[0] / size),
-    Math.floor(rgb[1] / size),
-    Math.floor(rgb[2] / size)
-  ];
+  return [Math.floor(rgb[0] / size), Math.floor(rgb[1] / size), Math.floor(rgb[2] / size)]
 }
